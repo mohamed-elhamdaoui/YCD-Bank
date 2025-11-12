2fdc395d86e5e98f97d869e0fcf1f4074c70a9d1
Mariam982440 <mariam1elhaimer@gmail.com>
2025-11-12 10:04:25 +0100

    enregistrement des virements de l'epargne et localStorage

--- assets/js/epargne.js
+++ assets/js/epargne.js
@@ -1,5 +1,10 @@
 let soldeVisible= true;
 
+window.onload=function(){
+
+    this.document.querySelector('form').onsubmit= enregistrerVirementEpargne;
+}
+
 document.getElementById('toggleSolde').addEventListener('click', function () {
     soldeVisible=!soldeVisible;
     const soldeEpargne=document.getElementById('soldeEpargne');
@@ -21,4 +26,40 @@ document.getElementById('btnVirement').addEventListener('click', function(){
     const popupForm= document.getElementById('formulaire');
     popupForm.classList.remove('hidden');
 })
+document.getElementById('closePopup').addEventListener('click', function(){
+    const popupForm= document.getElementById('formulaire');
+    popupForm.classList.add('hidden');
+})
+document.getElementById('cancelBtn').addEventListener('click', function(){
+    const popupForm= document.getElementById('formulaire');
+    popupForm.classList.add('hidden');
+})
+function enregistrerVirementEpargne(e){
+    e.preventDefault();
+
+    const montant=document.getElementById('montantVirement').value;
+    const description=document.getElementById('descriptionVirement').value;
+    if (!montant || !description) {
+        alert('Veuillez remplir tous les champs');
+        return;
+    }
+
+
+    const virment = JSON.parse(localStorage.getItem("virement"))  || []
+    
+    console.log(virment) ;
+
+    
+    const virementEpargne= {
+        description: "virement_vers_principal",
+        montant: montant,
+        date: new Date().toLocaleString()
+    }
+
+virment.push(virementEpargne) ;
+
+localStorage.setItem("virement",JSON.stringify(virment))
+const popupForm= document.getElementById('formulaire');
+popupForm.classList.add('hidden');
+}
  
