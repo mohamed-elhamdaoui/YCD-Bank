494905c7377b7b844b589852e3b78835bebd7694
Mariam982440 <mariam1elhaimer@gmail.com>
2025-11-14 20:08:58 +0100

    fix: reglage du dark mode page historique

--- assets/js/historique.js
+++ assets/js/historique.js
@@ -9,7 +9,7 @@ let html=``;
 for(let i=0; i<tableHistorique.length ; i++){
     const V = tableHistorique[i];
     
-    html+=`<div class="flex justify-between text-center  border-4  shadow-md rounded-xl bg-red-100 w-[90%] py-3 px-12">
+    html+=`<div class="flex justify-between text-center  border-4  shadow-md rounded-xl bg-red-100 w-[90%] py-3 px-12 dark:bg-gray-900 dark:text-white">
             <div class="flex flex-col justify-start">
                 <div class="text-[120%] font-bold mb-3 ">${V.type}</div>
                 <div class="text-[70%]  mb-2">${V.date}</div>
